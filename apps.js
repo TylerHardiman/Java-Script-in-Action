f05d71c947e60800ce08b0c0f5d3f4cb76f6975a
Tyler Hardiman <Fulldadmode89@gmail.com>
2022-01-31 16:42:29 -0500

    Finished Variables on JavaScript in Action Worksheet.

--- apps.js
+++ apps.js
@@ -1,8 +1,15 @@
-console.log; {"Hello World"};
+let dayOfWeek = 'Monday';
+console.log( 'Monday' );
+let lastDayOfWeek = 'Friday';
+console.log('I cant wait until Friday!')
 
-function alertUserName(){
-let myName = prompt{"Please enter your name!"};
 
-alert; {myName}:
 
-}
+
+let userAnimalInput = prompt('What is your favorite animal? ')
+console.log(prompt)
+let userColorInput = prompt('What is your favorite color? ')
+let favoriteColorAnimal = userColorInput + userAnimalInput
+console.log(prompt('Ive never seen a', favoriteColorAnimal))
+
+
